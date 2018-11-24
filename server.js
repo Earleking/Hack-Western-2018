d8b24cf1f81f6fd357cd33e5df916721d6cdf73d
Gautam Gupta <35315739+ggupta24@users.noreply.github.com>
2018-11-24 03:52:13 -0500

    Functionality into function

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
-var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
-var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-    'version': '2018-03-16',
-    'iam_apikey': "QhHkZQwZquq60Je3wUq6yGO_S23J3NjdH-5qtHO-mpMX",
-    'url': "https://gateway-wdc.watsonplatform.net/natural-language-understanding/api"
-});
+function getKeywords(data){
+    var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+    var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+        'version': '2018-03-16',
+        'iam_apikey': "QhHkZQwZquq60Je3wUq6yGO_S23J3NjdH-5qtHO-mpMX",
+        'url': "https://gateway-wdc.watsonplatform.net/natural-language-understanding/api"
+    });
 
-var parameters = {
-    'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
-    'features': {
-        'entities': {
-        },
-        'keywords': {
-            'limit': 5
-        }
-    }
-}
-
-var keywords_list = [];
-natural_language_understanding.analyze(parameters, function (err, response) {
-    if (err)
-        console.log('error:', err);
-    else
-        console.log(JSON.stringify(response.keywords[0].text));
-
-        for(var i = 0; i < response.length; i ++){
-            if(response.keywords[i].relevance >= 0.6){
-                keywords_list.push(response.keywords[i].text);
-                console.log(response.keywords[i].text);
+    var parameters = {
+        'text': data,
+        'features': {
+            'entities': {
+            },
+            'keywords': {
+                'limit': 5
             }
         }
+    }
 
-        console.log(keywords_list[0]);
-       for(var i = 0; i < keywords_list.length; i++){
-            console.log(keywords_list[i]);
-        };
-});
+    var keywords_list = [];
+    natural_language_understanding.analyze(parameters, function (err, response) {
+        if (err)
+            console.log('error:', err);
+        else
+            //console.log(JSON.stringify(response.keywords.length));
+
+            for(var i = 0; i < response.keywords.length; i ++){
+                if(JSON.stringify(response.keywords[0].relevance) >= 0.6){
+                    keywords_list.push(JSON.stringify(response.keywords[i].text));
+                    console.log(keywords_list[i]);
+                }
+            }
+    });
+
+    return keywords_list;
+}
